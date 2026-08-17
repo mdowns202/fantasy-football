eaca4889bd37ac9eb780b9b8e1c20b0935b4e552
Mark Downs <m.downs1994@gmail.com>
2026-08-17 13:53:59 -0400

    updated vite.config.js base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [deno(), solid(), tailwindcss()],
-  base: "/the-great-show", 
+  base: "/the-great-show/", 
   server: {
     host: "10.100.0.3",
     port: 8202,
